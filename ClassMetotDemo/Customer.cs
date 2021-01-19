2789084cb5cf9f8eb83c5a020404f62b86bf0167
Caglan Atay <caglanatay@windowslive.com>
2021-01-19 03:59:58 +0300

    created features

--- ClassMetotDemo/Customer.cs
+++ ClassMetotDemo/Customer.cs
@@ -6,5 +6,12 @@ namespace ClassMetotDemo
 {
     class Customer
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mail { get; set; }
+        public double Account { get; set; }
+        public string Branch { get; set; }
+
     }
 }
